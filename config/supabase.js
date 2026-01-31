22acc2d57cbe514156145dbb3d62d34898bae706
yerralalitha <yerralalitha91@gmail.com>
2026-01-31 20:33:43 +0530

    Initial setup and server running

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,6 @@
-const { createClient }=require("@supabase/supabase-js");
 require("dotenv").config();
-const supabase=createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABSE_KEY
-);
+const { createClient }=require("@supabase/supabase-js");
+const supabaseUrl=process.env.SUPABASE_URL;
+const supabaseKey=process.env.SUPABASE_KEY;
+const supabase=createClient(supabaseUrl,supabaseKey);
 module.exports=supabase;
